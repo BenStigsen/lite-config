1caa53ddd40f919454f7a580671fc7b3edacff46
BenStigsen <benjamin.stigsen@gmail.com>
2021-01-25 14:54:51 +0100

    Added themechooser.lua

---
+++ plugins/themechooser.lua
@@ -0,0 +1,20 @@
+local core = require "core"
+local command = require "core.command"
+
+command.add(nil, {
+  ["themechooser:choose-theme"] = function()
+    core.command_view:enter("Set Theme To", function(text)
+      local theme = text:match("^%s*(.-)%s*$")
+      local file = loadfile(("./data/user/colors/%s.lua"):format(theme))
+
+      if file ~= nil then
+        file()
+        core.log("Changed theme to \"%s\"", theme)
+        core.redraw = true
+      else
+        core.log("Error loading theme \"%s\"", theme)
+      end
+    end)
+  end,
+})
+
